96e00571d42dd02a82faf2348683c7b54893edbb
EwoutMaertens <maertens.ewout@gmail.com>
2023-12-07 23:28:02 +0100

    Adding solution to sixth exercise + updating template to include performance benchmark results

--- e6_wait_for_it/wait_for_it.py
+++ e6_wait_for_it/wait_for_it.py
@@ -1,4 +1,6 @@
 from typing import Callable, List, Any
+import math
+from functools import reduce
 from time import perf_counter
 
 # GENERAL
@@ -13,21 +15,49 @@ def process_input_file(input_file: str, line_processor: Callable) -> List[Any]:
             line = f.readline()
     return result_list
 
+def calculate_distance_travelled(time_waited: int, total_time: int) -> int:
+    return (total_time - time_waited) * time_waited
+
 # PART ONE
 
 def process_input_line_p1(line: str) -> Any:
-    pass
+    return [int(val) for val in line.split(':')[1].strip().split()]
 
 def process_result_list_p1(result_list: List[Any]) -> Any:
-    pass
+    times = result_list[0]
+    distances = result_list[1]
+    scores = []
+    for i in range(len(times)):
+        time = times[i]
+        distance = distances[i]
+        time_middle = math.ceil(time/2)
+
+        right_boundary = time
+        left_boundary = time_middle
+        scoring_value = time_middle
+
+        while left_boundary + 1 != right_boundary:
+            middle_element = left_boundary + math.floor((right_boundary - left_boundary + 1) / 2)
+            
+            if calculate_distance_travelled(middle_element, time) > distance:
+                left_boundary = middle_element
+                scoring_value = middle_element
+            else:
+                right_boundary = middle_element
+
+        non_scoring_values = time - scoring_value
+        scoring_values = scoring_value - non_scoring_values + 1
+        scores.append(scoring_values)
+
+    return reduce(lambda x, y: x*y, scores)
 
 # PART TWO
 
 def process_input_line_p2(line: str) -> Any:
-    pass
+    return [int("".join([val for val in line.split(':')[1].strip().split()]))]
 
 def process_result_list_p2(result_list: List[Any]) -> Any:
-    pass
+    return process_result_list_p1(result_list)
 
 # MAIN
 
